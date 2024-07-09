2d7adf32ceae4160357fc2cd45bb654de383b4c0
SaadYasin <qatar.horizon2009@gmail.com>
2024-07-09 17:10:59 +0300

    commit two

---
+++ .history/tailwind.config_20240708010309.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1.5rem",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    fontFamily: {
+      primary: "Poppins",
+      secondary: "Jost",
+    },
+    backgroundImage: {
+      hero_l: "url('/assets/hero/hero01-02.jpg')",
+      hero_s: "url('/assets/hero/hero01-03.jpg')",
+      hero: "url('/assets/hero/hero01-02.jpg')",
+      // grid: "url('/assets/grid.png')",
+    },
+    extend: {
+      colors: {
+        darkBg: {
+          DEFAULT: "#03252f",
+        },
+        primary: {
+          DEFAULT: "#013d5a",
+          hover: "#343e4a",
+        },
+        secondary: "#4d5053",
+        accent: {
+          DEFAULT: "#cda274",
+          secondary: "#f4f0ec",
+          hover: "#b88c5d",
+        },
+      },
+      
+    },
+  },
+  plugins: [],
+};
+
